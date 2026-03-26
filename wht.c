d1f15a8b16bc94c449d90acfcd779a2f9726d671
Andrew Consroe <aconz2@gmail.com>
2026-03-26 16:29:48 -0500

    add a different 128 impl

--- wht.c
+++ wht.c
@@ -67,7 +67,7 @@ void NOINLINE wht_16_simd(float* x) {
 
 
 void NOINLINE wht_32_simd(float* x) {
-    const size_t k = 4;
+    const size_t k = 4; // number of vectors
     const __m256 sign[3] = {
         _mm256_set_ps(-1., +1., -1., +1., -1., +1., -1, +1.),
         _mm256_set_ps(-1., -1., +1., +1., -1., -1., +1, +1.),
@@ -109,8 +109,8 @@ void NOINLINE wht_32_simd(float* x) {
     }
 }
 
-void NOINLINE wht_64_simd(float* x) {
-    const size_t k = 8;
+void wht_64_simd_(float* x) {
+    const size_t k = 8; // number of vectors
     const __m256 sign[3] = {
         _mm256_set_ps(-1., +1., -1., +1., -1., +1., -1, +1.),
         _mm256_set_ps(-1., -1., +1., +1., -1., -1., +1, +1.),
@@ -154,9 +154,13 @@ void NOINLINE wht_64_simd(float* x) {
     }
 }
 
+void NOINLINE wht_64_simd(float* x) {
+    wht_64_simd_(x);
+}
+
 // this spills to stack, not sure there is any way around that
 void NOINLINE wht_128_simd(float* x) {
-    const size_t k = 16;
+    const size_t k = 16; // number of vectors
     const __m256 sign[3] = {
         _mm256_set_ps(-1., +1., -1., +1., -1., +1., -1, +1.),
         _mm256_set_ps(-1., -1., +1., +1., -1., -1., +1, +1.),
@@ -200,11 +204,36 @@ void NOINLINE wht_128_simd(float* x) {
     }
 }
 
-void print(float x[32]) {
-    for (int i = 0; i < 32; i++) {
-        printf("%g ", x[i]);
+// this is another ordering where we do all on one half, then the other, then the last round
+// compiler leaves the second half in registers so maybe less stack spilling
+// 47 load/stores vs 67 for the above
+// is maybe 10% faster but don't really trust the numbers
+void NOINLINE wht_128_simd_alt(float* x) {
+    const size_t k = 16; // number of vectors
+
+    wht_64_simd_(x);
+    wht_64_simd_(x + 64);
+
+    const size_t h = 8;
+    // this is round 7
+    __m256 a, aa, b, bb;
+    for (size_t i = 0; i < k; i += h * 2) {
+        for (size_t j = i; j < i + h; j++) {
+            a = _mm256_loadu_ps(&x[j * 8]);
+            b = _mm256_loadu_ps(&x[(j + h) * 8]);
+            aa = _mm256_add_ps(a, b);
+            bb = _mm256_sub_ps(a, b);
+            _mm256_storeu_ps(&x[j * 8], aa);
+            _mm256_storeu_ps(&x[(j + h) * 8], bb);
+        }
     }
-    printf("\n");
+}
+
+void print(float* x, size_t n) {
+    /*for (int i = 0; i < n; i++) {*/
+    /*    printf("%g ", x[i]);*/
+    /*}*/
+    /*printf("\n");*/
 }
 
 // just messed around until got a non-trivial output
@@ -224,50 +253,28 @@ void check_eq(float* x, float* y, size_t n) {
 }
 
 int main() {
-    float x[128];
-    float y[128];
+    // use sized arrays to let asan detect bounds
+    float x16[16], x32[32], x64[64], x128[128];
+    float y16[16], y32[32], y64[64], y128[128];
 
 #ifdef TEST
 
-    printf("n=16\n");
-    test_case_1(x, 16);
-    print(x);
-    wht_n_ref(x, 16);
-    print(x);
-    test_case_1(y, 16);
-    wht_16_simd(y);
-    print(y);
-    check_eq(x, y, 16);
+#define CASE(f, N) \
+    printf("n=%d\n", N); \
+    test_case_1(x##N, N); \
+    print(x##N, N); \
+    wht_n_ref(x##N, N); \
+    print(x##N, N); \
+    test_case_1(y##N, N); \
+    f(y##N); \
+    print(y##N, N); \
+    check_eq(x##N, y##N, N);
 
-    printf("\nn=32\n");
-    test_case_1(x, 32);
-    print(x);
-    wht_n_ref(x, 32);
-    print(x);
-    test_case_1(y, 32);
-    wht_32_simd(y);
-    print(y);
-    check_eq(x, y, 32);
-
-    printf("\nn=64\n");
-    test_case_1(x, 64);
-    print(x);
-    wht_n_ref(x, 64);
-    print(x);
-    test_case_1(y, 64);
-    wht_64_simd(y);
-    print(y);
-    check_eq(x, y, 64);
-
-    printf("\nn=128\n");
-    test_case_1(x, 128);
-    print(x);
-    wht_n_ref(x, 128);
-    print(x);
-    test_case_1(y, 128);
-    wht_128_simd(y);
-    print(y);
-    check_eq(x, y, 128);
+    CASE(wht_16_simd, 16)
+    CASE(wht_32_simd, 32)
+    CASE(wht_64_simd, 64)
+    CASE(wht_128_simd, 128)
+    CASE(wht_128_simd_alt, 128)
 
 #else
 
@@ -275,71 +282,82 @@ int main() {
     float acc;
     Timespec start, stop;
 
-    test_case_1(x, 32);
+    test_case_1(x32, 32);
     acc = 0;
     clock_ns(&start);
     for (size_t i = 0; i < rounds; i++) {
-        memcpy(y, x, sizeof(float) * 32);
-        acc += x[3];
+        memcpy(y32, x32, sizeof(float) * 32);
+        acc += x32[3];
     }
     clock_ns(&stop);
     printf("%20s acc=%f elapsed=%ldns per_iter=%.2fns\n", "overhead_32", acc, elapsed_ns(start, stop), (double)elapsed_ns(start, stop) / rounds);
     uint64_t remove = elapsed_ns(start, stop);
 
-    test_case_1(x, 32);
+    test_case_1(x32, 32);
     acc = 0;
     clock_ns(&start);
     for (size_t i = 0; i < rounds; i++) {
-        memcpy(y, x, sizeof(float) * 32);
-        wht_n_ref(y, 32);
-        acc += y[3];
+        memcpy(y32, x32, sizeof(float) * 32);
+        wht_n_ref(y32, 32);
+        acc += y32[3];
     }
     clock_ns(&stop);
     printf("%20s acc=%f elapsed=%ldns per_iter=%.2fns\n", "wht_32_ref", acc, elapsed_ns(start, stop), (double)(elapsed_ns(start, stop) - remove) / rounds);
 
-    test_case_1(x, 16);
+    test_case_1(x16, 16);
     acc = 0;
     clock_ns(&start);
     for (size_t i = 0; i < rounds; i++) {
-        memcpy(y, x, sizeof(float) * 16);
-        wht_16_simd(y);
-        acc += y[3];
+        memcpy(y16, x16, sizeof(float) * 16);
+        wht_16_simd(y16);
+        acc += y16[3];
     }
     clock_ns(&stop);
     printf("%20s acc=%f elapsed=%ldns per_iter=%.2fns\n", "wht_16_simd", acc, elapsed_ns(start, stop), (double)(elapsed_ns(start, stop) - remove) / rounds);
 
-    test_case_1(x, 32);
+    test_case_1(x32, 32);
     acc = 0;
     clock_ns(&start);
     for (size_t i = 0; i < rounds; i++) {
-        memcpy(y, x, sizeof(float) * 32);
-        wht_32_simd(y);
-        acc += y[3];
+        memcpy(y32, x32, sizeof(float) * 32);
+        wht_32_simd(y32);
+        acc += y32[3];
     }
     clock_ns(&stop);
     printf("%20s acc=%f elapsed=%ldns per_iter=%.2fns\n", "wht_32_simd", acc, elapsed_ns(start, stop), (double)(elapsed_ns(start, stop) - remove) / rounds);
 
-    test_case_1(x, 64);
+    test_case_1(x64, 64);
     acc = 0;
     clock_ns(&start);
     for (size_t i = 0; i < rounds; i++) {
-        memcpy(y, x, sizeof(float) * 64);
-        wht_64_simd(y);
-        acc += y[3];
+        memcpy(y64, x64, sizeof(float) * 64);
+        wht_64_simd(y64);
+        acc += y64[3];
     }
     clock_ns(&stop);
     printf("%20s acc=%f elapsed=%ldns per_iter=%.2fns\n", "wht_64_simd", acc, elapsed_ns(start, stop), (double)(elapsed_ns(start, stop) - remove) / rounds);
 
-    test_case_1(x, 128);
+    test_case_1(x128, 128);
     acc = 0;
     clock_ns(&start);
     for (size_t i = 0; i < rounds; i++) {
-        memcpy(y, x, sizeof(float) * 128);
-        wht_128_simd(y);
-        acc += y[3];
+        memcpy(y128, x128, sizeof(float) * 128);
+        wht_128_simd(y128);
+        acc += y128[3];
     }
     clock_ns(&stop);
     printf("%20s acc=%f elapsed=%ldns per_iter=%.2fns\n", "wht_128_simd", acc, elapsed_ns(start, stop), (double)(elapsed_ns(start, stop) - remove) / rounds);
+
+    test_case_1(x128, 128);
+    acc = 0;
+    clock_ns(&start);
+    for (size_t i = 0; i < rounds; i++) {
+        memcpy(y128, x128, sizeof(float) * 128);
+        wht_128_simd_alt(y128);
+        acc += y128[3];
+    }
+    clock_ns(&stop);
+    printf("%20s acc=%f elapsed=%ldns per_iter=%.2fns\n", "wht_128_simd_alt", acc, elapsed_ns(start, stop), (double)(elapsed_ns(start, stop) - remove) / rounds);
 
 #endif
 }
